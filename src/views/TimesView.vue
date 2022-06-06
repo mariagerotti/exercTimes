1bfaca56d0756f7897f1af42e6e49caad111bcf1
mariagerotti <mariaeduarda.gerotti@gmail.com>
2022-06-06 09:05:31 -0300

    upload

--- src/views/TimesView.vue
+++ src/views/TimesView.vue
@@ -7,25 +7,52 @@
       <input type="text">
       <button>salvar</button>
     </div>
-
+    <div class="list-times">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>nome</th>
+            <th>ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>time1</td>
+            <td>??</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>time2</td>
+            <td>??</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>time3</td>
+            <td>??</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <style>
-.title{
+.title {
   margin: 2rem auto;
   display: flex;
   justify-content: center;
 }
 
-.form-input{
+.form-input {
   margin-top: 10px;
   display: flex;
   justify-content: center;
 
 }
 
-.form-input input{
+.form-input input {
   width: 60%;
   height: 40px;
   border: 1px solid #ccc;
@@ -33,7 +60,7 @@
   padding: 0 10px;
 }
 
-.form-input button{
+.form-input button {
   margin-left: 1%;
   width: 20%;
   height: 40px;
@@ -43,5 +70,27 @@
   color: white;
   font-weight: bold;
   cursor: pointer;
+}
+
+.list-times{
+  display: flex;
+  justify-content: center;
+}
+
+table{
+  width: 50%;
+  margin: 2% auto;
+  border-collapse: collapse;
+}
+
+table tr th{
+  border: 1px solid #ccc;
+  padding: 10px;
+  font-weight: bold;
+}
+
+table tr td{
+  border: 1px solid #ccc;
+  padding: 10px;
 }
 </style>
